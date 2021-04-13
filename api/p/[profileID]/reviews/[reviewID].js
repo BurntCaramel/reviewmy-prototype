1525b68b639ff1d24c1de27614bdab21e03d8536
Patrick Smith <pgwsmith@gmail.com>
2021-04-13 18:02:40 +1000

    404 when review is not found

--- api/p/[profileID]/reviews/[reviewID].js
+++ api/p/[profileID]/reviews/[reviewID].js
@@ -3,9 +3,17 @@ const { profiles, reviewsByProfileID } = require("../../../../src/data");
 
 function handleRequest(req, res) {
   const { profileID, reviewID } = req.query;
+  console.log({ profileID, reviewID });
   const profile = profiles.get(profileID);
   const reviews = reviewsByProfileID.get(profileID);
   const review = reviews.get(reviewID);
+
+  if (review === undefined) {
+    res.writeHead(404, "Not Found");
+    res.end("Review not found");
+    return;
+  }
+
   htmlWriter(res)(ProfileReviewPage({ profile, reviews }));
 }
 
