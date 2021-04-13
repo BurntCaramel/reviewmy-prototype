6690356f4aeef89f3e464dfbe004f431beb06b62
Patrick Smith <pgwsmith@gmail.com>
2021-04-13 16:37:13 +1000

    Update vercel routes

--- api/p/[profileID].js
+++ api/p/[profileID].js
@@ -1,7 +1,11 @@
 const { htmlWriter, ProfilePage } = require("../../src/view");
+const { profiles, reviewsByProfileID } = require("../../src/data");
 
 function handleRequest(req, res) {
-  htmlWriter(res)(ProfilePage());
+  const { profileID } = req.query;
+  const profile = profiles.get(profileID);
+  const reviews = reviewsByProfileID.get(profileID);
+  htmlWriter(res)(ProfilePage({ profile, reviews }));
 }
 
 module.exports = handleRequest;
